fe93e2f42993921700ed317582a0de05dc716f8a
pritam-demo <pritjon2002@gmail.com>
2026-09-25 09:06:53 +0545

    react fragment used

--- React1/src/App.jsx
+++ React1/src/App.jsx
@@ -4,10 +4,13 @@ import Title from './Title.jsx'
 function App() {
 
   return(
-    <div>
-      <h1>I am React App</h1>
-      <Title />
-    </div>
+    <>
+    <h1>I am React App</h1>
+    <Title />
+    <Title />
+    <Title />
+    </>
+
   )
 }
 
