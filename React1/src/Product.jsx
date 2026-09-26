6ce4ac8669189cd885a146a6339272a666adedd8
pritam-demo <pritjon2002@gmail.com>
2026-09-26 10:23:41 +0545

    dynamic component styling

--- React1/src/Product.jsx
+++ React1/src/Product.jsx
@@ -1,8 +1,9 @@
 import "./Product.css";
 
 function Product({title = "Heading" , description , lists}) {
+    let styles = {backgroundColor : description.length > 18 ? "red" : null }
   return (
-    <div className="Product">
+    <div className="Product" style={styles}>
       <h1>{title}</h1>
       {description.length > 18 ? <h2> <u>{description}</u> </h2> : <h2> {description} </h2> }
       <h3>{lists.map((para) => <li>{para}</li>)}</h3>
