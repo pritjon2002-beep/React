23dae441c02d35ef1b4270fa0c3b9f9539933c3c
pritam-demo <pritjon2002@gmail.com>
2026-09-26 21:19:03 +0545

    simple activity

--- React1/src/App.jsx
+++ React1/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import ProductTab from "./ProductTab.jsx";
 import Title from "./Title.jsx";
+import MessageBox from "./MsgBox.jsx";
 
 function App() {
   return (
@@ -8,6 +9,12 @@ function App() {
     <div>
       <Title />
     </div>
+
+     <div>
+        <MessageBox userName="Pritam" textColor="Red" />
+        <MessageBox userName="Priya" textColor="Blue" />
+        <MessageBox userName="Dd" textColor="Yellow" />
+      </div>
 
     <div>
       <ProductTab />
---
+++ React1/src/MsgBox.jsx
@@ -0,0 +1,6 @@
+export default function MessageBox({ userName, textColor }) {
+  return (
+<h1 style={{color: textColor }}>Hello {userName}</h1>
+  ) 
+}
+
