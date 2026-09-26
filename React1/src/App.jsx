9b5467a912d421c829d564d9454f8ca9e5aae5de
pritam-demo <pritjon2002@gmail.com>
2026-09-26 21:32:37 +0545

    clearing the files

--- React1/src/App.jsx
+++ React1/src/App.jsx
@@ -1,21 +1,9 @@
 import "./App.css";
 import ProductTab from "./ProductTab.jsx";
-import Title from "./Title.jsx";
-import MessageBox from "./MsgBox.jsx";
 
 function App() {
   return (
     <>
-    <div>
-      <Title />
-    </div>
-
-     <div>
-        <MessageBox userName="Pritam" textColor="Red" />
-        <MessageBox userName="Priya" textColor="Blue" />
-        <MessageBox userName="Dd" textColor="Yellow" />
-      </div>
-
     <div>
       <ProductTab />
     </div>
--- React1/src/Product.jsx
+++ React1/src/Product.jsx
@@ -1,12 +1,9 @@
 import "./Product.css";
 
-function Product({title = "Heading" , description , lists}) {
-    let styles = {backgroundColor : description.length > 18 ? "red" : null }
+function Product({}) {
   return (
-    <div className="Product" style={styles}>
-      <h1>{title}</h1>
-      {description.length > 18 ? <h2> <u>{description}</u> </h2> : <h2> {description} </h2> }
-      <h3>{lists.map((para) => <li>{para}</li>)}</h3>
+    <div className="Product">
+      <h1>Products</h1>
     </div>
   );
 }
