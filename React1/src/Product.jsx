5da1d5f4ca858437b25b2905044bf8f5425cef2f
pritam-demo <pritjon2002@gmail.com>
2026-09-26 09:29:24 +0545

    array in props

--- React1/src/Product.jsx
+++ React1/src/Product.jsx
@@ -1,10 +1,11 @@
 import "./Product.css";
 
-function Product({title = "Heading" , description}) {
+function Product({title = "Heading" , description , lists}) {
   return (
     <div className="Product">
       <h1>{title}</h1>
       <h2>{description}</h2>
+      <h3>{lists}</h3>
     </div>
   );
 }
--- React1/src/ProductTab.jsx
+++ React1/src/ProductTab.jsx
@@ -1,12 +1,13 @@
 import Product from "./Product";
 
 function ProductTab() {
+    let lists = ["para1" ,"para2" , "para3" ,"para4"];
   return (
     <>
-      <Product title="Heading 1 " description="first description " />
-      <Product title="Heading 2 " description=" second description" />
-      <Product title="Heading 3" description="third description" />
-      <Product  description="forth description" />
+      <Product title="Heading 1 " description="first description "  lists = {lists} />
+      <Product title="Heading 2 " description=" second description"  lists = {lists} />
+      <Product title="Heading 3" description="third description"  lists = {lists} />
+      <Product  description="forth description"  lists = {lists} />
     </>
   );
 }
