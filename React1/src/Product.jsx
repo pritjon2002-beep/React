677c8d8f5be906d44c0f5cf856ef95d798d387e9
pritam-demo <pritjon2002@gmail.com>
2026-09-26 09:39:26 +0545

    rendering arrays in li

--- React1/src/Product.jsx
+++ React1/src/Product.jsx
@@ -1,11 +1,12 @@
 import "./Product.css";
 
 function Product({title = "Heading" , description , lists}) {
+    const itemList = lists.map((para) => <li>{para}</li>)
   return (
     <div className="Product">
       <h1>{title}</h1>
       <h2>{description}</h2>
-      <h3>{lists}</h3>
+      <h3>{itemList}</h3>
     </div>
   );
 }
