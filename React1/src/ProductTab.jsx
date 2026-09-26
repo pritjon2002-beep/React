b7af6021f600ff01d8072e7413b0b67d0bbe8b26
pritam-demo <pritjon2002@gmail.com>
2026-09-26 10:00:04 +0545

    conditional statement

--- React1/src/ProductTab.jsx
+++ React1/src/ProductTab.jsx
@@ -1,10 +1,10 @@
 import Product from "./Product";
 
 function ProductTab() {
-    let lists = ["para1" ,"para2" , "para3" ,"para4"];
+    let lists = ["paragraph1" ,"para2" , "paragraph3" ,"para4"];
   return (
     <>
-      <Product title="Heading 1 " description="first description "  lists = {lists} />
+      <Product title="Heading 1 " description="firstttt description "  lists = {lists} />
       <Product title="Heading 2 " description=" second description"  lists = {lists} />
       <Product title="Heading 3" description="third description"  lists = {lists} />
       <Product  description="forth description"  lists = {lists} />
